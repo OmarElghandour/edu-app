f2679c80da9663eaad762fe1f8aa756e47168729
omarelghandour <omarelghandoor@gmail.com>
2020-04-11 08:59:27 +0200

    pre-heroku

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/test', function(req, res, next) {
     res.render('test', { title: 'Express' });
 });
 
