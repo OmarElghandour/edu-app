969ef69e5c26e5b9a82387362463da555ccc3434
omarelghandour <omarelghandoor@gmail.com>
2020-04-11 08:37:42 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,78 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017'
+mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    //...
+})
+
+const apiKey = '46513982';
+const apiSecret = 'd7d79d09ddfbcb962ca2879293281167f7bf1933';
+var OpenTok = require('opentok'),
+    opentok = new OpenTok(apiKey, apiSecret);
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/token', function (req, res) {
+   opentok.createSession(function(err, session) {
+        if (err) return console.log(err);
+
+        // token =  opentok.generateToken(session.sessionId);
+
+// Generate a Token from a session object (returned from createSession)
+        token = session.generateToken();
+      res.send({
+          session : session.sessionId,
+          token : token,
+      })
+    });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/index.js
@@ -0,0 +1,76 @@
+// replace these values with those generated in your TokBox Account
+const apiKey = '46513982';
+
+
+async function createSession(){
+    fetch('http://localhost:3000/token',{
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        },
+    }).then(function(response) {
+        if (response.ok) {
+            response.json().then(json => {
+                initializeSession(json);
+            });
+        }
+    });
+}
+
+function joinSession() {
+    var session = OT.initSession('46513982', localStorage.getItem('session'));
+    session.on('streamCreated', function(event) {
+        console.log('sssss')
+        session.subscribe(event.stream, 'subscriber', {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        }, handleError);
+    });
+    session.connect(localStorage.getItem('token'), function(error) {
+        // If the connection is successful, initialize a publisher and publish to the session
+        if (error) {
+            handleError(error);
+        } else {
+            console.log("Connected to the session.");
+        }
+    });
+
+}
+
+
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+    if (error) {
+        alert(error.message);
+    }
+}
+
+// (optional) add server code here
+
+function initializeSession(json) {
+    var session = OT.initSession(apiKey, json.session);
+    console.log(json.token)
+    // Subscribe to a newly created stream
+
+    localStorage.setItem('session',json.session);
+    localStorage.setItem('token',json.token);
+    // // Create a publisher
+    var publisher = OT.initPublisher('publisher', {
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+    }, handleError);
+
+    // Connect to the session
+    session.connect(json.token, function(error) {
+        // If the connection is successful, initialize a publisher and publish to the session
+        if (error) {
+            handleError(error);
+        } else {
+            session.publish(publisher, handleError);
+        }
+    });
+
+}
