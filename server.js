1c6106ea6800c572e8094513d41301ca21572a47
omarelghandour <omarelghandoor@gmail.com>
2020-05-16 02:48:36 +0200

    make it better

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/frontend/src/'));
 
-app.get('/*', function(req,res) {
+app.get('/frontend', function(req,res) {
 
     res.sendFile(path.join(__dirname+'/frontend/src/index.html'));
 });
