de2dbebcd25c1066f669220da95df132ddc0a095
omar elghandour <omar.elghandour@cobrowser.net>
2022-08-09 07:59:33 +0200

    feat: update router

--- app/app.js
+++ app/app.js
@@ -16,34 +16,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(cors());
-
-
-// app.get('/', (req, res) => {
-//   res.send('xxxxxxxxxxxxxxx');
-// });
-
-
-// const subscribersRouter = require('./routes/subscriberApi');
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-// const sessionApi = require('./routes/sessionApi');
-// const categoryApi = require('./routes/Category');
 const dataBase = require('./sqlModels/index');
 
 app.use('/', router);
-// app.use('/users', usersRouter);
-// app.use('/subscribers', subscribersRouter);
-// app.use('/session',sessionApi);
-// app.use('/category',categoryApi);
-
-
-
-
-app.use( (req,res,next)=>{
-  res.render('404');
-})
-
-
 
 app.listen(5000, async () => {
   console.log(`Example app listening at http://localhost:5000`)
